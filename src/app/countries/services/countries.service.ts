2f011753c6e7aeaa1f9b85288a09c3775bfba3ca
Sebastián Aguirre <guiablanco@live.cl>
2023-10-16 17:43:24 -0300

    Añadiendo servicios para búsqueda por país, región y capital

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -12,7 +12,39 @@ export class CountriesService {
 
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${ term }`;
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => {
+          console.log(error);
+
+          //Esto entrega un observable con un array vacío
+          return of([]); })
+      ) ;
+  }
+
+  searchCountry( term: string ): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${ term }`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => {
+          console.log(error);
+
+          //Esto entrega un observable con un array vacío
+          return of([]); })
+      ) ;
+  }
+
+  searchRegion( term: string ): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${ term }`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => {
+          console.log(error);
+          alert('No hay información con el parámetro buscado')
+
+          //Esto entrega un observable con un array vacío
+          return of([]); })
+      ) ;
   }
 
 }
